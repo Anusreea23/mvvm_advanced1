2103baf93d6cc90b8785ad1ee7c60cd600c45b07
Anusreea23 <anusreeijinaraj>
2024-10-22 15:35:52 +0530

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:mvvm_advanced1/services/api_service.dart';
+import 'package:mvvm_advanced1/ui/screens/splash/splash_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: SplashView, initial: true),
+  ],
+  dependencies: [
+    LazySingleton(classType: ApiService),
+    LazySingleton(classType: NavigationService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: BottomSheetService),
+  ],
+)
+class AppSetup {}
---
+++ lib/ui/screens/dummy/dummy_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_advanced1/ui/screens/dummy/dummy_viewmodel.dart';
+// import 'package:mvvm_boilerplate/ui/screens/dummy/dummy_viewmodel.dart';
+import 'package:stacked/stacked.dart';
+
+class DummyView extends StatelessWidget {
+  const DummyView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<DummyViewmodel>.reactive(
+      onViewModelReady: (viewModel) {}, // similar to init satate
+      onDispose: (viewModel) {},
+
+      builder: (context, viewModel, child) => Scaffold(),
+      viewModelBuilder: () => DummyViewmodel(),
+    );
+  }
+}
